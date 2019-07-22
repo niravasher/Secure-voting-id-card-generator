ba8661db9fbfe890f368db490ba19de13081e347
niravasher <f20171066@pilani.bits-pilani.ac.in>
2019-07-22 23:38:24 +0530

    integrated frontend with blockchain network and metamask

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -2,4 +2,19 @@ pragma solidity ^0.5.0;
 
 contract Cards {
     uint public voteCount = 0;
+
+    struct details {
+        string name;
+        uint date;
+        uint month;
+        bool completed;
+    }
+
+    constructor() public {
+        generateCard("nirav", 3, 8);
+    }
+
+    function generateCard(string memory _content, uint date, uint month) public {
+        details(_content, date, month, false);
+    }
 }
